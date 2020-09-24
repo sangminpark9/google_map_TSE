922c5e67168ad99a464141474382e0f9d1e4645e
박상민 <sangmin@bagsangmin-ui-MacBookPro.local>
2020-09-24 15:23:08 +0900

    first commit

---
+++ app/src/main/java/com/example/googlemapexample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.googlemapexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager=getFragmentManager();
+        mapFragment=(MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync( this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location=new LatLng(37.550762, 127.073911);//세종대학교 위치
+        MarkerOptions markerOptions =new MarkerOptions();
+        markerOptions.title("세종대학교");
+        markerOptions.snippet("연구실 위치");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,16));
+
+    }
+}
